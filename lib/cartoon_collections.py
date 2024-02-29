98ecf23e1a34dd9304539e0acf2798795d84cde1
RumiYo <yokoyamarumi@gmail.com>
2024-02-28 21:07:28 -0500

    Complete lab

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,22 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(names):
+    indexed_names = [f"{index+1}. {name}" for index, name in enumerate(names)]
+    names_together = "\n".join(indexed_names)
+    print(names_together)
 
-def summon_captain_planet():
-    pass
+def summon_captain_planet(calls):
+    updated_calls = [f"{call.capitalize()}!" for call in calls]
+    return updated_calls 
 
-def long_planeteer_calls():
-    pass
+def long_planeteer_calls(calls):
+    long_calls = list()
+    for i in calls:
+        if len(i) > 4:
+           long_calls.append(i)
+    return len(long_calls) > 0
 
-def find_the_cheese():
-    pass
+
+def find_the_cheese(items):
+    cheeses = ["cheddar", "gouda", "camembert"]
+    for i in items:
+        if i in cheeses:
+            return i
